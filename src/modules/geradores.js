8272a4af9cce38cbb5b8438851a0769e30e83b1d
Gustavo Rabelo <148560309+gustarabelo@users.noreply.github.com>
2024-01-24 12:17:39 -0300

    consertando erro

--- src/modules/geradores.js
+++ src/modules/geradores.js
@@ -1,9 +1,9 @@
 // console.log(String.fromCharCode(2))
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
-geraMaiuscula = () => String.fromCharCode(rand(65, 91));
-geraMinuscula = () => String.fromCharCode(rand(97, 123));
-geraNumero = () => String.fromCharCode(rand(48, 58));
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumero = () => String.fromCharCode(rand(48, 58));
 const simbolos = ',.;~^[]{}!?@#$%&*()-_=+';
 const geraSimbolo = () => simbolos[rand(0, simbolos.length)]
 
